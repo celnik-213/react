936576aedbb3819e5629759ed10258ad713deda7
celnik-213 <uT_4000@zsp3zamosc.pl>
2025-11-03 11:16:57 +0100

    task 7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import ItemList from './components/ItemList'; 
 import 'bootstrap/dist/css/bootstrap.css'; 
+import DarkModeToggle from './components/DarkModeToggle';
 
 function App() { 
 
@@ -8,9 +9,10 @@ function App() {
 
         <div className="container mt-4"> 
 
-            <h1>Witaj na stronie</h1> 
-
+            <h1>Task 6</h1> 
             <ItemList />
+            <h1>Task 7</h1>
+            <DarkModeToggle />
 
 
         </div> 
