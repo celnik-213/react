06d2253a27a0d1d59040fbbecdcf1222c80ec959
celnik-213 <uT_4000@zsp3zamosc.pl>
2025-11-03 11:11:13 +0100

    1-6

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'; 
+import ItemList from './components/ItemList'; 
+import 'bootstrap/dist/css/bootstrap.css'; 
+
+function App() { 
+
+    return ( 
+
+        <div className="container mt-4"> 
+
+            <h1>Witaj na stronie</h1> 
+
+            <ItemList />
+
+
+        </div> 
+
+    ); 
+
+} 
+
+ 
+
+export default App; 
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const ItemList = () => {
+  const [isLightOn, setIsLightOn] = useState(true);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = isLightOn ? "#FFFFFF" : "#000000";
+  }, [isLightOn]);
+
+  const toggleLight = () => {
+    setIsLightOn(!isLightOn);
+  };
+
+  return (
+    <div
+      style={{
+        width: "auto",
+        height: "auto",
+        border: "1px solid black",
+        padding: "10px",
+        textAlign: "center",
+        backgroundColor: isLightOn ? "#FFFFFF" : "#000000",
+        color: isLightOn ? "#000000" : "#FFFFFF",
+      }}
+    >
+      <p>{isLightOn ? " Światło włączone" : " Światło zgaszone"}</p>
+      <button onClick={toggleLight}>Włącznik światła</button>
+    </div>
+  );
+};
+
+export default ItemList;
