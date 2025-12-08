33c8cfe735c4f2563801a59a22ac2ddf4a664eb6
celnik-213 <uT_4000@zsp3zamosc.pl>
2025-12-08 13:46:27 +0100

    Rozdzial10Task2Update

--- src/components/KomponentCardChildren.jsx
+++ src/components/KomponentCardChildren.jsx
@@ -4,9 +4,11 @@ import PropTypes from "prop-types";
 const Card = ({children, className=""}) => {
   return <div className={`card ${className}`.trim()} 
     style={{
-        border: "7px solid #ccc", 
+        border: "7px solid #373633", 
         padding: "9px", 
-        borderRadius: "8px"}}>
+        borderRadius: "8px",
+        backgroundColor: "#a5e0e6",
+        color: "#d300d6"}}>
     {children}</div>;
 };
 
