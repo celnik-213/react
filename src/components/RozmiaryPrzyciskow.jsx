1e39618be28eea7e7e3580cc598862412420d369
celnik-213 <uT_4000@zsp3zamosc.pl>
2025-12-09 12:40:08 +0100

    Rozdzial12Task2Update2

--- src/components/RozmiaryPrzyciskow.jsx
+++ src/components/RozmiaryPrzyciskow.jsx
@@ -13,13 +13,14 @@ const Button = ({ text="Kliknij mnie!" }) => {
 
     return (
         <div>
-            <p style={{ fontSize: size === 'small' ? '12px' : size === 'medium' ? '16px' : '20px' }}>{text}</p>
-            <div style={{ marginTop: '16px' }}>
+            <div style={{ marginTop: '16px' }}></div>
+                <button style={{ fontSize: size === 'small' ? '12px' : size === 'medium' ? '16px' : '20px' }} className={`btn btn-primary ${sizeClass}`}>{text}</button>
+                <br/>
                 <button style={{ backgroundColor: "red" }} onClick={() => setSize('small')} className="btn btn-secondary">Small</button>
                 <button style={{ backgroundColor: "green" }} onClick={() => setSize('medium')} className="btn btn-secondary">Medium</button>
                 <button style={{ backgroundColor: "blue" }} onClick={() => setSize('large')} className="btn btn-secondary">Large</button>
-            </div>
         </div>
+    
     );
 };
 
